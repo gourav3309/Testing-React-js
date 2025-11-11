8b31cef14080a4e97f87891c503329b3ea1d9b40
gourav3309 <gourav.sharma@ets.apeejay.edu>
2025-11-11 17:55:31 +0530

    ok first change via mac

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Code Updated on office Github Account.
         </p>
         <a
           className="App-link"
